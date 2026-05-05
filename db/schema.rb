4d78327c01588aeefd6cf5c4be2dc63311be29b6
Dean Hildebrand <dean.hildebrand88@gmail.com>
2026-05-04 22:08:51 -0400

    update ruby/rails versions, fixed startup errors

--- db/schema.rb
+++ db/schema.rb
@@ -2,68 +2,66 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_202326) do
-
+ActiveRecord::Schema[8.1].define(version: 2020_04_04_202326) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "favorite_players", force: :cascade do |t|
-    t.integer "user_id"
+    t.datetime "created_at", null: false
     t.integer "player_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "favorite_teams", force: :cascade do |t|
-    t.integer "user_id"
+    t.datetime "created_at", null: false
     t.integer "team_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "players", force: :cascade do |t|
-    t.string "name"
-    t.string "position"
+    t.datetime "created_at", null: false
     t.string "date_of_birth"
+    t.string "name"
     t.string "nationality"
-    t.string "shirt_number"
+    t.string "position"
     t.string "role"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "shirt_number"
     t.integer "team_id"
+    t.datetime "updated_at", null: false
   end
 
   create_table "teams", force: :cascade do |t|
-    t.string "full_name"
-    t.string "short_name"
-    t.string "tla"
-    t.string "crestUrl"
     t.string "address"
-    t.string "phone"
-    t.string "website"
+    t.string "club_colors"
+    t.datetime "created_at", null: false
+    t.string "crestUrl"
     t.string "email"
     t.string "founded"
-    t.string "club_colors"
+    t.string "full_name"
+    t.string "phone"
+    t.string "short_name"
+    t.string "tla"
+    t.datetime "updated_at", null: false
     t.string "venue"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "website"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "bio"
     t.string "avatar"
+    t.string "bio"
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.string "username"
   end
-
 end
